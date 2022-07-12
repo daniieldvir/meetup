c3cab24dcea5bbde33fa1266e5436e65c773e54f
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2022-07-12 18:16:24 +0300

    Layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'title1',
+    image:
+      'https://lp-cms-production.imgix.net/2019-06/8ec64b64e1d0805b1101f6c70c7f5b31-tel-aviv.jpg',
+    address: 'address',
+    description: 'description',
+  },
+  {
+    id: 'm2',
+    title: 'title2',
+    image:
+      'https://lp-cms-production.imgix.net/2019-06/8ec64b64e1d0805b1101f6c70c7f5b31-tel-aviv.jpg',
+    address: 'address',
+    description: 'description',
+  },
+];
+
 function HomePage() {
-  return <h1>hi</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
