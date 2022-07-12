7f66c2cf95019d8505157f1a7d81737eaf4c8c87
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2022-07-12 18:34:58 +0300

    MeetupDetail

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://lp-cms-production.imgix.net/2019-06/8ec64b64e1d0805b1101f6c70c7f5b31-tel-aviv.jpg"
+      title="title"
+      address="address"
+      description="description"></MeetupDetail>
+  );
+}
+
+export default MeetupDetails;
